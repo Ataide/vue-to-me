f750444308f92778916d982412e6e3c023d93088
Ataide Bastos <ataide.bastos@gmail.com>
2023-04-24 18:56:12 -0300

    Iniciando Projeto

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
